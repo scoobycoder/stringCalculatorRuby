e9c01449711660936050f8fc97762564ac124ff1
scoobycoder <scoobycoder@gmail.com>
2016-02-21 18:18:41 -0500

    second degenerate test case of nothing being passed to sum

--- calc_spec.rb
+++ calc_spec.rb
@@ -10,10 +10,14 @@ describe 'Be a Calculator' do
     expect(@calc.sum(['a', 1])).to eq(1)
   end
 
+  it 'Sum should return 0 when nothing is passed to it' do
+    expect(@calc.sum()).to eq(0)
+  end
+
 end
 
 class StringCalc
-  def sum(numbers=[])
+  def sum(numbers=[0])
     filtered_numbers = numbers.select{|number| number.is_a?(Fixnum)}
     filtered_numbers.first
   end
