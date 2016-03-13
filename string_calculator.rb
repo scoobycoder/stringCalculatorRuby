3f1b79883fee43a30c32782ace29aca5ef235c2b
scoobycoder <scoobycoder@gmail.com>
2016-03-13 17:01:01 -0400

    moving code to a class file out of the spec file

---
+++ string_calculator.rb
@@ -0,0 +1,53 @@
+class StringCalc
+  def sum(numbers=0)
+    return 0 if numbers == 0
+    convert_to_integers(numbers).inject(0, :+)
+  end
+
+  private
+
+  def convert_to_integers(numbers)
+    new_numbers = split_with_delimiter(numbers).collect { |number| number.to_i }
+    handle_special_numbers(new_numbers)
+  end
+
+  def handle_special_numbers(new_numbers)
+    raise_exception_on_negative_numbers(new_numbers)
+    filter_out_numbers_larger_than_1000(new_numbers)
+  end
+
+  def filter_out_numbers_larger_than_1000(new_numbers)
+    new_numbers.select { |number| number < 1001}
+  end
+
+  def raise_exception_on_negative_numbers(new_numbers)
+    negatives = select_negative_numbers(new_numbers)
+    print_negatives(negatives)
+    raise "No Negative Numbers Allowed: #{negatives.to_s}" if negatives.length > 0
+  end
+
+  def print_negatives(negatives)
+    puts "Negative numbers: #{negatives.to_s}" if negatives.length > 0
+  end
+
+  def select_negative_numbers(new_numbers)
+    new_numbers.select { |number| number < 0 }
+  end
+
+  def split_with_delimiter(numbers)
+    new_numbers = remove_new_line(numbers)
+    delimiter = find_delimiter(new_numbers)
+    return new_numbers.split(delimiter)
+  end
+
+  def find_delimiter(new_numbers)
+    delimiter_offset = 2
+    delimiter_location = new_numbers.index('//') + delimiter_offset
+    delimiter = new_numbers[delimiter_location]
+    return delimiter
+  end
+
+  def remove_new_line(numbers)
+    numbers.gsub('\n', '')
+  end
+end
